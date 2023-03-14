e62beaa60c798c47858c0c04555ab7f1e751fce5
ZackaryDenson <zackary.denson@ufl.edu>
2023-03-14 03:13:11 -0400

    Added sapphire and its relative blocks and items

---
+++ src/main/java/net/brockstar/legacymod/item/custom/WolfBodyArmorItem.java
@@ -0,0 +1,11 @@
+package net.brockstar.legacymod.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class WolfBodyArmorItem extends Item {
+    public WolfBodyArmorItem(Properties properties) {
+        super(properties);
+    }
+
+
+}
